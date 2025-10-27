19459e98bff5590de2c024ead904c98603aff2ad
jamesdr-10 <james_depena@ucne.edu.do>
2025-10-27 19:28:42 -0400

    Agregando contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P2_AP1_JamesDePena.Models;
+
+namespace P2_AP1_JamesDePena.DAL;
+
+public class Contexto : DbContext
+{
+    public DbSet<Registro> Registro { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+}
