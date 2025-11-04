fb0340002c79f7233238d8dd8d6dc264955f1547
jamesdr-10 <james_depena@ucne.edu.do>
2025-11-03 20:58:27 -0400

    Agregando validaciones al pedido

--- Models/Pedidos.cs
+++ Models/Pedidos.cs
@@ -6,8 +6,15 @@ public class Pedidos
 {
     [Key]
     public int PedidoId { get; set; }
+
+    [Required(ErrorMessage = "La fecha es obligatoria.")]
     public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [RegularExpression(@"^[a-zA-ZÑñáéíóúÁÉÍÓÚ\s]+$", ErrorMessage = "El nombre solo puede contener letras y espacios.")]
     public string NombreCliente { get; set; } = string.Empty;
+
+    [Range(1, double.MaxValue, ErrorMessage = "El monto debe ser mayor que 0.")]
     public decimal Total { get; set; }
     public ICollection<PedidosDetalle> PedidosDetalles { get; set; } = new List<PedidosDetalle>();
 }
